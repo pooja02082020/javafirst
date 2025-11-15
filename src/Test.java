a6b58b03dda9b8f1c48e0ae512e4d36668a16aff
pooja02082020 <lohanipooja77@gmail.com>
2025-11-15 12:11:31 -0500

    exception handling

--- src/Test.java
+++ src/Test.java
@@ -54,6 +54,13 @@ public class Test {
          boolean res = p++ == 7 && ++p == 9 || p++ == 9;
          System.out.println("p = " + p);
          System.out.println("result = " + res);
+         
+         Error obj = new Error();
+//         boolean flag1 = obj instanceof RuntimeException; //Line n1
+//         boolean flag2 = obj instanceof Exception; //Line n2
+         boolean flag3 = obj instanceof Error; //Line n3
+         boolean flag4 = obj instanceof Throwable; //Line n4
+         System.out.println(flag3 + ":" + flag4);
  
      }
 }
