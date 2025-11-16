dc7d59b276a0e5ea3fd724d95677e23aeb54681a
pooja02082020 <lohanipooja77@gmail.com>
2025-11-16 13:48:00 -0500

    strings

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,12 @@
 //import java.io.FileNotFoundException;
  
 public class Test1 {
+	  static double d1 =6.6;
+	     static int x =(int)d1;
+	 
      public static void main(String[] args) {
+    	 System.out.println("HELLO");
+
          try {
              System.out.println(1);
          } catch (NullPointerException ex) {
@@ -10,5 +15,16 @@ public class Test1 {
 //             System.out.println("TWO");   Java doesn't allow to catch specific checked exceptions if these are not thrown by the statements inside try block.
          }
          System.out.println("THREE");
+         
+         
+         int start = 1;
+         int sum = 0;
+         do {
+             if(start % 2 == 0) {
+                 continue;
+             }
+             sum += start;
+         } while(++start <= 10);
+         System.out.println(sum);
      }
 }
