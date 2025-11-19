920ed29d02cde370d0f36c6949a5be1902be4bef
pooja02082020 <lohanipooja77@gmail.com>
2025-11-18 20:45:56 -0500

    class and objects

---
+++ src/classesandobjects/Car.java
@@ -0,0 +1,17 @@
+package classesandobjects;
+
+public class Car {
+
+	String brandName;
+	String color;
+	int speed;
+	
+	void start() {
+		System.out.println("Car of "+color+" and "+brandName+ " is starting");
+	}
+	
+	void run() {
+		System.out.println("Car of "+color+" and "+speed+ " is running");
+	}
+
+}
---
+++ src/classesandobjects/TestCar.java
@@ -0,0 +1,22 @@
+package classesandobjects;
+
+public class TestCar {
+	public static void main(String[] args) {
+		Car c1 = new Car();
+		c1.brandName = "Tata";
+		c1.color = "Black";
+		c1.speed = 200;
+		
+		c1.start();
+		c1.run();
+		
+		
+		Car c2 = new Car();
+		c2.brandName = "BMW";
+		c2.color = "white";
+		c2.speed = 180;
+		
+		c2.start();
+		c2.run();
+	}
+}
