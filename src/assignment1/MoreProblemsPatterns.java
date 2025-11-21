97749bf3901605fd61703e158af0c34203314194
pooja02082020 <lohanipooja77@gmail.com>
2025-11-21 10:17:01 -0500

    assignments

--- src/assignment1/MoreProblemsPatterns.java
+++ src/assignment1/MoreProblemsPatterns.java
@@ -1,5 +1,7 @@
 package assignment1;
 
+import java.util.Scanner;
+
 public class MoreProblemsPatterns {
 
 	public static void main(String[] args) {
@@ -87,15 +89,162 @@ public class MoreProblemsPatterns {
 //			System.out.println();
 //		}
 
-		for (int i = 1; i <= 5; i++) {
-			for (int j = 1; j <= i; j++) {
-				if (j == 1 || j == i || i == 5)
-					System.out.print("*");
-				else
-					System.out.print(" ");
-			}
-			System.out.println();
-		}
+//		for (int i = 1; i <= 5; i++) {
+//			for (int j = 1; j <= i; j++) {
+//				if (j == 1 || j == i || i == 5)
+//					System.out.print("*");
+//				else
+//					System.out.print(" ");
+//			}
+//			System.out.println();
+//		}
+
+//		 for (int i = 1; i <= 4; i++) {
+//	            // left stars
+//	            for (int j = 1; j <= i; j++) {
+//	                System.out.print("*");
+//	            }
+//	            // spaces
+//	            for (int j = 1; j <= 8 - 2 * i; j++) {
+//	                System.out.print(" ");
+//	            }
+//	            // right stars
+//	            for (int j = 1; j <= i; j++) {
+//	                System.out.print("*");
+//	            }
+//	            System.out.println();
+//	        }
+//	        // lower part
+//	        for (int i = 3; i >= 1; i--) {
+//	            // left stars
+//	            for (int j = 1; j <= i; j++) {
+//	                System.out.print("*");
+//	            }
+//	            // spaces
+//	            for (int j = 1; j <= 8 - 2 * i; j++) {
+//	                System.out.print(" ");
+//	            }
+//	            // right stars
+//	            for (int j = 1; j <= i; j++) {
+//	                System.out.print("*");
+//	            }
+//	            System.out.println();
+//	        }
+
+//		Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter number to check: ");
+//        int num = sc.nextInt();
+//
+//        int original = num;
+//        int reverse = 0;
+//
+//        while (num > 0) {
+//            reverse = reverse * 10 + (num % 10);
+//            num /= 10;
+//        }
+//
+//        if (original == reverse)
+//            System.out.println(original +" is a Palindrome");
+//        else
+//            System.out.println(original + " is not Palindrome");
+//        sc.close();
+
+//		for (int i = 1; i <= 3; i++) {
+//			for (int j = 1; j <= 5; j++) {
+//				if (j % 2 == 0)
+//					System.out.print("#");
+//				else
+//					System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+
+//		for (int i = 1; i <= 9; i++) {
+//
+//            if (i == 1 || i == 9) {
+//                System.out.println("*");
+//            } else {
+//                for (int j = 1; j <= 3; j++) {
+//                    if (j == 1 || j == 3) {
+//                        System.out.print("*");
+//                    } else {
+//                        System.out.print(" ");
+//                    }
+//                }
+//                System.out.println();
+//            }
+//        }
+
+//		for (int i = 5; i >= 1; i--) {
+//			for (int j = 1; j <= (2 * i - 1); j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		// Lower half
+//		for (int i = 2; i <= 5; i++) {
+//			for (int j = 1; j <= (2 * i - 1); j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+//		 char ch = 'A';   
+//	        for (int i = 1; i <= 4; i++) {
+//	            for (int j = 1; j <= i; j++) {
+//	                System.out.print(ch);
+//	            }
+//	            System.out.println();
+//	            ch++; 
+//	        }
+   
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter the size n to create checkboard pattern");
+//		int n = sc.nextInt();
+//	        for (int i = 1; i <= n; i++) {
+//	            for (int j = 1; j <= n; j++) {
+//	            	if((i+j)%2==0)
+//	            		System.out.print(0);
+//	            	else
+//	            		System.out.print(1);
+//	            }
+//	            System.out.println();
+//	        }
+//	        sc.close();
+		
+//		Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter number of rows: ");
+//        int n = sc.nextInt();
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                if (j == 1 || j == i) {  //border 1
+//                    System.out.print("1 ");
+//                } else {
+//                    System.out.print("0 ");
+//                }
+//            }
+//
+//            System.out.println();
+//        }
+//        sc.close();
+		
+		  Scanner sc = new Scanner(System.in);
+		  System.out.println("Enter the number to create: ");
+	        int n = sc.nextInt();
+	        for (int i = 1; i <= n; i++) {
+	            for (int j = 1; j <= i; j++) {
+	                if (j == 1 || j == i) {
+	                    System.out.print("1 ");
+	                } else {
+	                    if (i % 2 == 0)
+	                        System.out.print("1 ");
+	                    else
+	                        System.out.print("0 ");
+	                }
+	            }
+	            System.out.println();
+	        }
+	        sc.close();
 
 	}
 }
