97480598a1c39fc02d54eb47ccf8e75621e77781
pooja02082020 <lohanipooja77@gmail.com>
2025-11-26 23:03:01 -0500

    multi threading

---
+++ src/multithreading/RunnableDemo.java
@@ -0,0 +1,29 @@
+package multithreading;
+
+class MyThread1 implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Thread Running: " + Thread.currentThread().getName());
+    }
+}
+
+public class RunnableDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+    	//Thread t1 = new Thread(new myThread1());
+    	Thread t = new Thread(new MyThread1());
+        t.start();
+        
+        t.sleep(1000);
+  
+        System.out.println("Thread Running 0"+ t.isAlive());
+        
+        MyThread1 mt1 = new MyThread1();
+        Thread t2 = new Thread(mt1);
+        t2.start();
+        
+        System.out.println("Thread Running 1"+ t.isAlive());
+    }
+}
--- src/multithreading/ThreadsDemo.java
+++ src/multithreading/ThreadsDemo.java
@@ -1,13 +1,21 @@
 package multithreading;
 
-class MyThread extends Thread{
- 
+class MyThread extends Thread {
+
 	public void run() {
-		System.out.println("Thread  Running");
+		System.out.println("Thread  Running" + Thread.currentThread().getName());
 	}
 }
 
 public class ThreadsDemo {
 
+	public static void main(String[] args) {
+		MyThread mt = new MyThread();
+		mt.start();
+		
+		MyThread mt1 = new MyThread();
+		mt1.start();
+	//start() method will cal the run method
+	}
 
 }
