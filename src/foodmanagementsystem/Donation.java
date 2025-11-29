72ceb927086f30e2a902364c12b2dfa7f96c7445
pooja02082020 <lohanipooja77@gmail.com>
2025-11-29 12:43:05 -0500

    added user, dination and reciepent fields

---
+++ src/foodmanagementsystem/Donation.java
@@ -0,0 +1,32 @@
+package foodmanagementsystem;
+
+public class Donation {
+
+	private int id;
+	private String foodItem;
+	private int quality;
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getFoodItem() {
+		return this.foodItem;
+	}
+	
+	public void setFoodItem(String foodItem) {
+		this.foodItem = foodItem;
+	}
+	
+	public int getuality() {
+		return this.quality;
+	}
+	
+	public void setQuality(int quality) {
+		this.quality = quality;
+	}
+}
---
+++ src/foodmanagementsystem/Recipient.java
@@ -0,0 +1,23 @@
+package foodmanagementsystem;
+
+public class Recipient {
+
+	private int id;
+	private String name;
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
