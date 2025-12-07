eefd4ea0127394475f3f9fec6fae6c7e25d529b2
pooja02082020 <lohanipooja77@gmail.com>
2025-12-07 17:02:31 -0500

    oops concepts

---
+++ src/oopsbasics/Counter.java
@@ -0,0 +1,9 @@
+package oopsbasics;
+
+class Counter {
+    static int count = 0;
+
+    Counter() {
+        count++;
+    }
+}
---
+++ src/oopsbasics/Student.java
@@ -0,0 +1,15 @@
+package oopsbasics;
+
+class Student {
+    String name;
+    int age;
+
+    Student() {
+        this.name = "Unknown";
+    }
+
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
